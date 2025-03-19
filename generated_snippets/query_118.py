1a4795a80195a3cdbc1473d36bcc66b3942f90a9
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 13:42:23 -0400

    Added snippet for step 7 of task: ''')

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,8 +1,20 @@
-# Generated: 2025-03-19 13:40:42.211583
-# Result: [([1, 4, 9, 16],)]
+# Generated: 2025-03-19 13:42:23.216268
+# Result: [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,)]
 # Valid: True
 import duckdb
 
+# Connect to in-memory database
 conn = duckdb.connect(':memory:')
-result = conn.execute('SELECT array_transform([1, 2, 3, 4], x -> x * x) as squared_array').fetchall()
+
+# Create and populate recursive CTE for generating number series
+result = conn.execute('''
+WITH RECURSIVE
+  number_series(n) AS (
+    SELECT 1
+    UNION ALL
+    SELECT n + 1 FROM number_series WHERE n < 10
+  )
+SELECT * FROM number_series
+''').fetchall()
+
 print(result)
