6d2fcdc0425391f8dfad9d70643b740a861967cb
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 15:19:22 -0400

    Added snippet for step 7 of task: SELECT array_transform([2, 3, 4, 5], x -> x * x) as squared_array

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,18 +1,8 @@
-# Generated: 2025-03-19 15:18:33.213167
-# Result: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+# Generated: 2025-03-19 15:19:22.598610
+# Result: [4, 9, 16, 25]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
-
-# Recursive Common Table Expression (CTE) to generate sequence
-result = conn.execute('''
-    WITH RECURSIVE number_sequence(n) AS (
-        SELECT 1
-        UNION ALL
-        SELECT n + 1 FROM number_sequence WHERE n < 10
-    )
-    SELECT n, n * n AS squared FROM number_sequence
-''').fetchall()
-
-print(result)  # Generates numbers 1-10 with their squares using recursive CTE
+result = conn.execute("""SELECT array_transform([2, 3, 4, 5], x -> x * x) as squared_array""").fetchone()[0]
+print(result)
