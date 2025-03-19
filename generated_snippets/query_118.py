fa294497e63bac96c5804d4516a0e7eb8ede86c5
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 17:53:03 -0400

    Added snippet for step 6 of task: rel = con.query('''

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,12 +1,30 @@
-# Generated: 2025-03-19 17:52:10.721603
-# Result: [([11, 12, 13, 14, 15],)]
+# Generated: 2025-03-19 17:53:03.409960
+# Result: [(5,), (2,), (1,), (3,), (4,)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
 
+# Create and populate recursive graph data
+conn.execute('''CREATE TABLE graph_edges (
+    source INTEGER,
+    destination INTEGER
+)''')
+
+conn.executemany('INSERT INTO graph_edges VALUES (?, ?)', [
+    (1, 2), (2, 3), (3, 4), (1, 4), (4, 5)
+])
+
+# Use recursive CTE to find all reachable nodes from node 1
 result = conn.execute('''
-    SELECT array_transform([1, 2, 3, 4, 5], x -> x + 10) AS increased_array
+WITH RECURSIVE path_search(node) AS (
+    SELECT source FROM graph_edges WHERE source = 1
+    UNION
+    SELECT e.destination
+    FROM graph_edges e
+    JOIN path_search ps ON e.source = ps.node
+)
+SELECT DISTINCT node FROM path_search
 ''').fetchall()
 
-print(result)  # Expected output: [[11, 12, 13, 14, 15]]
+print(result)  # Will show reachable nodes from node 1
