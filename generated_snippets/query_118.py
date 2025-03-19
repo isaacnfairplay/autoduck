b2f08e26ae554b73a3c527f9035616cf0adee646
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 17:45:57 -0400

    Added snippet for step 9 of task: print(rel.execute().fetchall())

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,8 +1,8 @@
-# Generated: 2025-03-19 17:42:36.957356
+# Generated: 2025-03-19 17:45:57.008380
 # Result: [([4, 9, 16, 25],)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
-result = conn.sql("SELECT array_transform([2, 3, 4, 5], x -> x * x) as squared_array").fetchall()
-print(result)
+rel = conn.sql('SELECT array_transform([1, 2, 3, 4, 5], x -> x * x) as squared_array')
+print(rel.execute().fetchall())
