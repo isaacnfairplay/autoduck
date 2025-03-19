41f95e444b8b897af23e168f946f879e27fae2cd
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 16:07:04 -0400

    Added snippet for step 7 of task: SELECT array_transform([2, 3, 4, 5], x -> x * x) as squared_array

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,20 +1,8 @@
-# Generated: 2025-03-19 16:06:15.322765
-# Result: [(0,), (1,), (1,), (2,), (3,), (5,), (8,), (13,), (21,), (34,), (55,)]
+# Generated: 2025-03-19 16:07:03.928555
+# Result: [([4, 9, 16, 25],)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
-
-# Recursive Common Table Expression (CTE) to generate Fibonacci sequence
-result = conn.execute('''
-WITH RECURSIVE fibonacci(n, a, b) AS (
-    SELECT 0, 0, 1
-    UNION ALL
-    SELECT n + 1, b, a + b
-    FROM fibonacci
-    WHERE n < 10
-)
-SELECT a AS fibonacci_number FROM fibonacci
-''').fetchall()
-
-print(result)  # Will print first 10 Fibonacci numbers
+result = conn.execute("SELECT array_transform([2, 3, 4, 5], x -> x * x) as squared_array").fetchall()
+print(result)
