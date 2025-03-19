b523c42e560de227e63bcd3663b3cd40996ded49
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 16:52:09 -0400

    Added snippet for step 8 of task: print(rel.execute().fetchall())

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,34 +1,11 @@
-# Generated: 2025-03-19 16:51:18.992899
+# Generated: 2025-03-19 16:52:08.995262
 # Result: [(1, datetime.datetime(2023, 6, 1, 10, 0), 22.5, 22.5), (1, datetime.datetime(2023, 6, 1, 11, 0), 23.100000381469727, 22.800000190734863), (1, datetime.datetime(2023, 6, 1, 12, 0), 23.700000762939453, 23.40000057220459), (2, datetime.datetime(2023, 6, 1, 10, 0), 21.299999237060547, 21.299999237060547)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
+conn.execute('CREATE TABLE numbers (value INT)')
+conn.executemany('INSERT INTO numbers VALUES (?)', [(x,) for x in range(1, 6)])
 
-# Create sensor temperature readings with time series analysis
-conn.execute('''
-    CREATE TABLE sensor_readings (
-        sensor_id INT,
-        reading_time TIMESTAMP,
-        temperature FLOAT
-    );
-''')
-
-conn.executemany('INSERT INTO sensor_readings VALUES (?, ?, ?)', [
-    (1, '2023-06-01 10:00:00', 22.5),
-    (1, '2023-06-01 11:00:00', 23.1),
-    (1, '2023-06-01 12:00:00', 23.7),
-    (2, '2023-06-01 10:00:00', 21.3)
-])
-
-# Analyze temperature trend using window functions
-result = conn.execute('''
-    SELECT 
-        sensor_id, 
-        reading_time,
-        temperature,
-        AVG(temperature) OVER (PARTITION BY sensor_id ORDER BY reading_time ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) as moving_avg
-    FROM sensor_readings
-''').fetchall()
-
-print(result)
+rel = conn.sql('SELECT value, value * value as squared FROM numbers')
+print(rel.execute().fetchall())
