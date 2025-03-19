d5fbda16a491ac870c6ced2f8198530b82afadfc
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 17:53:53 -0400

    Added snippet for step 7 of task: SELECT array_transform([1, 2, 3, 4], x -> x + 10) as incremented_array

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,30 +1,12 @@
-# Generated: 2025-03-19 17:53:03.409960
-# Result: [(5,), (2,), (1,), (3,), (4,)]
+# Generated: 2025-03-19 17:53:53.424085
+# Result: [([11, 12, 13, 14],)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
 
-# Create and populate recursive graph data
-conn.execute('''CREATE TABLE graph_edges (
-    source INTEGER,
-    destination INTEGER
-)''')
-
-conn.executemany('INSERT INTO graph_edges VALUES (?, ?)', [
-    (1, 2), (2, 3), (3, 4), (1, 4), (4, 5)
-])
-
-# Use recursive CTE to find all reachable nodes from node 1
 result = conn.execute('''
-WITH RECURSIVE path_search(node) AS (
-    SELECT source FROM graph_edges WHERE source = 1
-    UNION
-    SELECT e.destination
-    FROM graph_edges e
-    JOIN path_search ps ON e.source = ps.node
-)
-SELECT DISTINCT node FROM path_search
+    SELECT array_transform([1, 2, 3, 4], x -> x + 10) as incremented_array
 ''').fetchall()
 
-print(result)  # Will show reachable nodes from node 1
+print(result)  # Expected output: [[11, 12, 13, 14]]
