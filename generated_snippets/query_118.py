9014abf86a51da7932a4d1b4e88b2b9bff1b6bfd
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 13:12:35 -0400

    Added snippet for step 7 of task: SELECT array_transform([1, 2, 3, 4], x -> x * 2) as doubled_array

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,18 +1,8 @@
-# Generated: 2025-03-19 13:11:46.709875
-# Result: [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,)]
+# Generated: 2025-03-19 13:12:35.619647
+# Result: [([2, 4, 6, 8],)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
-
-# Create a recursive Common Table Expression (CTE) to generate a sequence
-result = conn.execute('''
-WITH RECURSIVE sequence(n) AS (
-    SELECT 1
-    UNION ALL
-    SELECT n + 1 FROM sequence WHERE n < 10
-)
-SELECT * FROM sequence
-''').fetchall()
-
-print(result)  # Will print numbers 1 through 10
+result = conn.execute('SELECT array_transform([1, 2, 3, 4], x -> x * 2) as doubled_array').fetchall()
+print(result)
