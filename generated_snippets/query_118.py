eb48662038d11268cd1983aba54300ecd821ff50
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 16:06:15 -0400

    Added snippet for step 6 of task: rel = con.query('''

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,13 +1,20 @@
-# Generated: 2025-03-19 16:05:23.827008
-# Result: [([1, 4, 9, 16, 25],)]
+# Generated: 2025-03-19 16:06:15.322765
+# Result: [(0,), (1,), (1,), (2,), (3,), (5,), (8,), (13,), (21,), (34,), (55,)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
 
-# Transform numeric array by squaring each element
-result = conn.execute("""
-    SELECT array_transform([1, 2, 3, 4, 5], x -> x * x) AS squared_values
-""").fetchall()
+# Recursive Common Table Expression (CTE) to generate Fibonacci sequence
+result = conn.execute('''
+WITH RECURSIVE fibonacci(n, a, b) AS (
+    SELECT 0, 0, 1
+    UNION ALL
+    SELECT n + 1, b, a + b
+    FROM fibonacci
+    WHERE n < 10
+)
+SELECT a AS fibonacci_number FROM fibonacci
+''').fetchall()
 
-print(result)  # Output: [[1, 4, 9, 16, 25]]
+print(result)  # Will print first 10 Fibonacci numbers
