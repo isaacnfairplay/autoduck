df0f0be9c5ffc75dff4e72400a5e2fbdfaef588b
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 18:03:19 -0400

    Added snippet for step 7 of task: SELECT array_transform([1, 2, 3, 4], x -> x + 10) as incremented_array

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,18 +1,12 @@
-# Generated: 2025-03-19 18:02:29.631034
-# Result: [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,)]
+# Generated: 2025-03-19 18:03:18.984255
+# Result: [([11, 12, 13, 14],)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
 
-# Recursive common table expression (CTE) to generate a numeric sequence
 result = conn.execute('''
-    WITH RECURSIVE number_series(n) AS (
-        SELECT 1
-        UNION ALL
-        SELECT n + 1 FROM number_series WHERE n < 10
-    )
-    SELECT * FROM number_series
+    SELECT array_transform([1, 2, 3, 4], x -> x + 10) as incremented_array
 ''').fetchall()
 
-print(result)  # Demonstrates recursive query for generating sequential numbers
+print(result[0][0])
