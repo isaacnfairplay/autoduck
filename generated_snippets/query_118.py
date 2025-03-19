98bd2b5eca9652fcc5e6a623b07c0b4b33106031
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 18:02:29 -0400

    Added snippet for step 6 of task: rel = con.query('''

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,12 +1,18 @@
-# Generated: 2025-03-19 18:01:39.695258
-# Result: [([15, 25, 35, 45],)]
+# Generated: 2025-03-19 18:02:29.631034
+# Result: [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
 
+# Recursive common table expression (CTE) to generate a numeric sequence
 result = conn.execute('''
-    SELECT array_transform([5, 15, 25, 35], x -> x + 10) AS transformed_array
+    WITH RECURSIVE number_series(n) AS (
+        SELECT 1
+        UNION ALL
+        SELECT n + 1 FROM number_series WHERE n < 10
+    )
+    SELECT * FROM number_series
 ''').fetchall()
 
-print(result[0][0])  # Outputs: [15, 25, 35, 45]
+print(result)  # Demonstrates recursive query for generating sequential numbers
