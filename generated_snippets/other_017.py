1e87605b023bc168090e43c1c2fee057cfd96357
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-18 20:10:27 -0400

    Added snippet for step 5 of task: con = duckdb.connect()

---
+++ generated_snippets/other_017.py
@@ -0,0 +1,7 @@
+# Generated: 2025-03-18 20:10:26.549027
+# Result: [('Electronics', 'Laptop', Decimal('999.99'), 799.99, 1), ('Electronics', 'Smartphone', Decimal('599.99'), 799.99, 2), ('Sports', 'Running Shoes', Decimal('129.99'), 129.99, 3)]
+# Valid: True
+# Variable con: Type: DuckDBPyConnection
+# Attributes/Methods: _pybind11_conduit_v1_(), append(), array_type(), arrow(), begin(), checkpoint(), close(), commit(), create_function(), cursor(), decimal_type(), description, df(), dtype(), duplicate(), enum_type(), execute(), executemany(), extract_statements(), fetch_arrow_table(), fetch_df(), fetch_df_chunk(), fetch_record_batch(), fetchall(), fetchdf(), fetchmany(), fetchnumpy(), fetchone(), filesystem_is_registered(), from_arrow(), from_csv_auto(), from_df(), from_parquet(), from_query(), get_table_names(), install_extension(), interrupt(), list_filesystems(), list_type(), load_extension(), map_type(), pl(), query(), read_csv(), read_json(), read_parquet(), register(), register_filesystem(), remove_function(), rollback(), row_type(), rowcount, sql(), sqltype(), string_type(), struct_type(), table(), table_function(), tf(), torch(), type(), union_type(), unregister(), unregister_filesystem(), values(), view()
+import duckdb
+con = duckdb.connect()
