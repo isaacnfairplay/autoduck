1ab31438f4da495e260b2b16d05cca4aa6ce7ae0
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 10:40:58 -0400

    Added snippet for step 8 of task: SELECT DATE '2023-01-01'

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,21 +1,8 @@
-# Generated: 2025-03-19 10:40:08.486712
-# Result: [(datetime.date(2024, 1, 1),), (datetime.date(2024, 2, 1),), (datetime.date(2024, 3, 1),), (datetime.date(2024, 4, 1),), (datetime.date(2024, 5, 1),), (datetime.date(2024, 6, 1),), (datetime.date(2024, 7, 1),), (datetime.date(2024, 8, 1),), (datetime.date(2024, 9, 1),), (datetime.date(2024, 10, 1),), (datetime.date(2024, 11, 1),), (datetime.date(2024, 12, 1),), (datetime.date(2025, 1, 1),)]
+# Generated: 2025-03-19 10:40:57.923623
+# Result: (datetime.date(2023, 1, 1),)
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
-
-# Generate monthly fiscal date series for 2024 with start and end bounds
-result = conn.execute("""
-WITH RECURSIVE date_series(fiscal_month) AS (
-    SELECT DATE '2024-01-01'
-    UNION ALL
-    SELECT fiscal_month + INTERVAL 1 MONTH
-    FROM date_series
-    WHERE fiscal_month < DATE '2024-12-31'
-)
-SELECT fiscal_month
-FROM date_series
-""").fetchall()
-
-print([str(date[0]) for date in result])
+result = conn.execute("SELECT DATE '2023-01-01'").fetchone()
+print(result[0])
