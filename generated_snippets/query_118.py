ddc7983273e1628521a9a3b5fdf423e63ae9f966
Isaac Moore <isaacmooreuky@gmail.com>
2025-03-19 21:14:45 -0400

    Added snippet for step 8 of task: print(rel.execute().fetchall())

--- generated_snippets/query_118.py
+++ generated_snippets/query_118.py
@@ -1,18 +1,8 @@
-# Generated: 2025-03-19 21:13:54.829638
+# Generated: 2025-03-19 21:14:45.169634
 # Result: [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,)]
 # Valid: True
 import duckdb
 
 conn = duckdb.connect(':memory:')
-
-# Recursive common table expression to generate number series
-result = conn.sql("""
-WITH RECURSIVE number_series(n) AS (
-    SELECT 1
-    UNION ALL
-    SELECT n + 1 FROM number_series WHERE n < 10
-)
-SELECT * FROM number_series
-""").fetchall()
-
-print(result)
+rel = conn.sql('SELECT range(10) AS numbers')
+print(rel.execute().fetchall())
